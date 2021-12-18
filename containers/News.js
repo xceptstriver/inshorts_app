2608fd792633a7304a32a4687c5d2e124051dcd4
xceptstriver <a.n.v.k.s.sai@gmail.com>
2021-12-18 21:47:56 +0530

    [news screen with snap carousel]

--- containers/News.js
+++ containers/News.js
@@ -1,18 +1,44 @@
-import React, {useContext} from 'react';
-import {View, Text} from 'react-native';
+import React, {useContext, useState} from 'react';
+import {View, Text, Dimensions, StyleSheet} from 'react-native';
+import Carousel from 'react-native-snap-carousel';
 import {NewsContext} from '../API/context';
+import SingleNews from '../components/SingleNews';
+const windowHeight = Dimensions.get('window').height;
 
 const News = () => {
   const {
     news: {articles},
   } = useContext(NewsContext);
-  console.log('artcles hai bhai', articles);
+  const [activeIndex, setActiveIndex] = useState();
 
   return (
-    <View>
-      <Text>News Screen</Text>
+    <View style={styles.carousel}>
+      {articles && (
+        <Carousel
+          layout="stack"
+          data={articles.slice(0, 10)}
+          sliderHeight={300}
+          itemHeight={windowHeight}
+          vertical={true}
+          removeClippedSubviews={false}
+          renderItem={({item, index}) => (
+            <SingleNews item={item} index={index} />
+          )}
+          enableMomentum={false}
+          decelerationRate={'fast'}
+          onSnapToItem={index => setActiveIndex(index)}
+        />
+      )}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  carousel: {
+    flex: 1,
+    backgroundColor: 'black',
+    transform: [{scaleY: -1}],
+  },
+});
 
 export default News;
